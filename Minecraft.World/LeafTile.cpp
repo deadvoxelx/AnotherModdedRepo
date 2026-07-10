beafd9942fb1025e2f9b043185d52a71570df916
DeadVoxelx <deadvoxelx@gmail.com>
2026-07-10 06:00:17 -0400

    Update LeafTile.cpp

--- Minecraft.World/LeafTile.cpp
+++ Minecraft.World/LeafTile.cpp
@@ -137,11 +137,11 @@ void LeafTile::tick(Level *level, int x, int y, int z, Random *random)
 					for (int yo = -r; yo <= r; yo++)
 					{
 						int t = level->getTile(x + xo, y + yo, z + zo);
-						if (t == Tile::treeTrunk_Id)
+						if (t == Tile::treeTrunk_Id || t == Tile::cherryLog_Id || t == Tile::palmLog_Id || t == Tile::mossyLog_Id)
 						{
 							checkBuffer[(xo + WO) * WW + (yo + WO) * W + (zo + WO)] = 0;
 						}
-						else if (t == Tile::leaves_Id)
+						else if (t == Tile::leaves_Id || t == Tile::leaves2_Id)
 						{
 							checkBuffer[(xo + WO) * WW + (yo + WO) * W + (zo + WO)] = -2;
 						}
