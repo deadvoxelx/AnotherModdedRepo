049a775ccb17023b1d5773f6edcc452e760264fd
DeadVoxelx <deadvoxelx@gmail.com>
2026-07-17 05:54:50 -0400

    Update StructureRecipies.cpp

--- Minecraft.World/StructureRecipies.cpp
+++ Minecraft.World/StructureRecipies.cpp
@@ -74,6 +74,24 @@ void StructureRecipies::addRecipes(Recipes *r)
 		L'#', Tile::cobblestone,
 		L'S');
 
+	r->addShapedRecipy(new ItemInstance(Tile::furnace), //
+		L"sssctg",
+		L"###", //
+		L"# #", //
+		L"###", //
+
+		L'#', Tile::limestoneCobbled,
+		L'S');
+
+	r->addShapedRecipy(new ItemInstance(Tile::furnace), //
+		L"sssctg",
+		L"###", //
+		L"# #", //
+		L"###", //
+
+		L'#', Tile::graniteCobbled,
+		L'S');
+
 	r->addShapedRecipy(new ItemInstance(static_cast<Tile *>(Tile::chest)), //
 		L"sssctg",
 		L"###", //
