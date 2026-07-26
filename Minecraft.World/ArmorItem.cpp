5149371e7ce83b4f61d204951ba2b7d8baba76b1
DeadVoxelx <deadvoxelx@gmail.com>
2026-07-26 06:40:18 -0400

    Update ArmorItem.cpp

--- Minecraft.World/ArmorItem.cpp
+++ Minecraft.World/ArmorItem.cpp
@@ -63,11 +63,13 @@ const int _ArmorMaterial::chainArray[] = {2, 5, 4, 1};
 const int _ArmorMaterial::ironArray[] = {2, 6, 5, 2};
 const int _ArmorMaterial::goldArray[] = {2, 5, 3, 1};
 const int _ArmorMaterial::diamondArray[] = {3, 8, 6, 3};
+const int _ArmorMaterial::steelArray[] = {2, 6, 5, 2};
 const _ArmorMaterial *_ArmorMaterial::CLOTH = new _ArmorMaterial(5, _ArmorMaterial::clothArray, 15);
 const _ArmorMaterial *_ArmorMaterial::CHAIN = new _ArmorMaterial(15, _ArmorMaterial::chainArray, 12);
 const _ArmorMaterial *_ArmorMaterial::IRON = new _ArmorMaterial(15, _ArmorMaterial::ironArray, 9);
 const _ArmorMaterial *_ArmorMaterial::GOLD = new _ArmorMaterial(7, _ArmorMaterial::goldArray, 25);
 const _ArmorMaterial *_ArmorMaterial::DIAMOND = new _ArmorMaterial(33, _ArmorMaterial::diamondArray, 10);
+const _ArmorMaterial *_ArmorMaterial::STEEL = new _ArmorMaterial(22, _ArmorMaterial::steelArray, 15);
 
 _ArmorMaterial::ArmorMaterial(int durabilityMultiplier, const int slotProtections[], int enchantmentValue)
 {
