d052e49fdf4c6279ec2779be4c295fe8659dc978
DeadVoxelx <deadvoxelx@gmail.com>
2026-08-25 10:59:05 -0400

    Update WeighedTreasure.h

--- Minecraft.World/WeighedTreasure.h
+++ Minecraft.World/WeighedTreasure.h
@@ -6,6 +6,8 @@ class WeighedTreasure : public WeighedRandomItem
 {
 private:
 	shared_ptr<ItemInstance> item;
+	int itemId;
+	int auxValue;
 	int minCount;
 	int maxCount;
 
@@ -13,6 +15,10 @@ public:
 	WeighedTreasure(int itemId, int auxValue, int minCount, int maxCount, int weight);
 	WeighedTreasure(shared_ptr<ItemInstance> item, int minCount, int maxCount, int weight);
 
+private:
+	shared_ptr<ItemInstance> createItem() const;
+
+public:
 	static void addChestItems(Random *random, WeighedTreasureArray items, shared_ptr<Container> dest, int numRolls);
 	static void addDispenserItems(Random *random, WeighedTreasureArray items, shared_ptr<DispenserTileEntity> dest, int numRolls);
 	static WeighedTreasureArray addToTreasure(WeighedTreasureArray items, WeighedTreasure *extra);
